3d60b0746a166c6863ca27ce18cb9dd0aefc251d
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-20 02:25:26 +0200

    Add view to handle members_login

--- members/views.py
+++ members/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def member_login(request):
+    return render(request, "authenticate/login.html", {})
