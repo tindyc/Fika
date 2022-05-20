2bc12b48276727eb3bcb6fde7ab2b024f2a27c79
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-20 05:07:22 +0200

    Add form-control class to custom django form fields

--- members/forms.py
+++ members/forms.py
@@ -4,9 +4,13 @@ from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    first_name = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    last_name = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={"class": "form-control"})
+    )
 
     class Meta:
         model = User
@@ -18,3 +22,10 @@ class RegisterUserForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs["class"] = "form-control"
+        self.fields["password1"].widget.attrs["class"] = "form-control"
+        self.fields["password2"].widget.attrs["class"] = "form-control"
