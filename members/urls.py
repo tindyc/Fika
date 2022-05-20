4febb8ccb17ac1300520ef0a085c72d77aa1e366
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-20 04:25:15 +0200

    Create route to map it with registration view

--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("login", views.member_login, name="login"),
     path("logout", views.member_logout, name="logout"),
+    path("register", views.member_registration, name="register"),
 ]
