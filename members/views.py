f5cc8980b8d3e2ce805678f122a02cd407fa8a5d
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-20 03:11:55 +0200

    Update view for member login

--- members/views.py
+++ members/views.py
@@ -4,4 +4,22 @@ from django.contrib import messages
 
 
 def member_login(request):
+    """Renders login page or logins the user depending on req. method"""
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect("home")
+        else:
+            # Return an 'invalid login' error message.
+            messages.warning(
+                request,
+                ("Invalid login, check your username & password before trying again."),
+            )
+            return redirect("login")
+
     return render(request, "authenticate/login.html", {})
