aa172f0c77525997454e6ab8a96503ab9072feb7
tindyc <tindyc@bath.edu>
2022-05-21 10:43:36 +0000

    Add view for about page

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path("about/", views.about, name='about'),
+
 ]
