c545179650a38c0cbd3aabc49bcd6d82a9f96e8f
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-22 03:09:17 +0200

    Create FriendRequest model

--- friend/models.py
+++ friend/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.utils import timezone
 
+
 # Create your models here.
 class FriendList(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user")
@@ -38,3 +39,51 @@ class FriendList(models.Model):
         if friend in self.friends.all():
             return True
         return False
+
+
+class FriendRequest(models.Model):
+    """
+    A friend request consists of two main parts:
+        1. SENDER - Person sending the friend req.
+        2. RECEIVER - Person receiving the friend request.
+    """
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
+
+    is_active = models.BooleanField(blank=True, null=False, default=True)
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sender.username
+    
+    def accept(self):
+        """
+        Accept a friend request.
+        Update both SENDER and RECEIVER friend list
+        """
+        receiver_friend_list = FriendList.objects.get(user=self.receiver)
+        
+        if receiver_friend_list:
+            receiver_friend_list.add_friend(self.sender)
+            sender_friend_list = FriendList.objects.get(user=self.sender)
+            if sender_friend_list:
+                sender_friend_list.add_friend(self.receiver)
+                self.is_active = False
+                self.save()
+    
+    def decline(self):
+        """
+        Decline a friend request.
+        It is "declined" by setting the 'is_active' field to false.
+        """
+        self.is_active = False
+        self.save()
+
+    def cancel(self):
+        """
+        Cancel a friend request
+        It is 'canceled' by setting the 'is_active' field to false.
+        """
+        self.is_active = False
+        self.save()
