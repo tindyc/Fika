03093f66a0649256b918522bcfdb3a7f5d2a6a0c
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-22 23:22:10 +0200

    Map route:view for friend request query

--- friend/urls.py
+++ friend/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("search/", views.friend_search_view, name="search"),
     path("friend_request/", views.send_friend_request, name="friend-request"),
+    path("friend_request/<user_id>", views.friend_requests_view, name="friend-requests"),
 ]
