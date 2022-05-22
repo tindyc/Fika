c0072751dc10f7164102aecc60bb8d42dd224dfd
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-23 01:03:43 +0200

    Map accept_req... view with the route

--- friend/urls.py
+++ friend/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("search/", views.friend_search_view, name="search"),
     path("friend_request/", views.send_friend_request, name="friend-request"),
     path("friend_request/<user_id>", views.friend_requests_view, name="friend-requests"),
+    path("accept_friend_request/<friend_request_id>", views.accept_friend_request, name="accept-friend-request"),
 ]
