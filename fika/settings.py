4c1e5d9f5a6de608266c66ceb61d1033c9ed68f9
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-22 19:11:00 +0200

    Fix typo & Add BASE_URL

--- fika/settings.py
+++ fika/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'home',
     'members',
     'contact',
-    'friend'
+    'friend',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +138,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_URL = "http://localhost:8000/"
