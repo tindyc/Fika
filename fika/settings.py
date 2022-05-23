ee0e7c1e0d1603f459f5c44cd0e3ab63a84b177a
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-23 16:05:45 +0200

    Fix allowed host typo

--- fika/settings.py
+++ fika/settings.py
@@ -37,7 +37,7 @@ if "DEVELOPMENT" in os.environ:
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ["https://fika-platform.herokuapp.com/," "localhost"]
+ALLOWED_HOSTS = ["https://fika-platform.herokuapp.com/,", "localhost"]
 
 
 # Application definition
