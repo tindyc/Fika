d3d7d5ab3ec31e48dbd96ca6d7c41693f6d44072
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-23 17:31:20 +0200

    Hot-fix key typo

--- fika/settings.py
+++ fika/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if "SECRET_KEY" in os.environ:
-    DEBUG = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 else:
     SECRET_KEY = 'django-insecure-+#x6p&pbdn649ak5mrp3ae8il9+2g2)d&4$cyoo9viots&ce9m'
 
