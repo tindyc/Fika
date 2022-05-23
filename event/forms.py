11e59db3b07de82180fdb189ffdee2a347b7fc38
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-23 05:20:20 +0200

    Omit columns from model and form

---
+++ event/forms.py
@@ -0,0 +1,25 @@
+from models import EventPost
+from django import forms
+
+
+class RegisterEventPostForm(forms.ModelForm):
+    title = forms.CharField(max_length=75, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.TextField(max_length=100, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
+    address = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
+    
+
+    class Meta:
+        model = EventPost
+        fields = (
+            "title",
+            "content",
+            "address",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterEventPostForm, self).__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs["class"] = "form-control"
+        self.fields["content"].widget.attrs["class"] = "form-control"
+        self.fields["address"].widget.attrs["class"] = "form-control"
+
--- event/models.py
+++ event/models.py
@@ -8,14 +8,7 @@ class EventPost(models.Model):
 
     title = models.CharField(max_length=75)
     content = models.TextField(max_length=100, null=True, blank=True)
-
     address = models.CharField(verbose_name="Address",max_length=100, null=True, blank=True)
-    town = models.CharField(verbose_name="Town/City",max_length=100, null=True, blank=True)
-    county = models.CharField(verbose_name="County",max_length=100, null=True, blank=True)
-    post_code = models.CharField(verbose_name="Post Code",max_length=8, null=True, blank=True)
-    country = models.CharField(verbose_name="Country",max_length=100, null=True, blank=True)
-    longitude = models.CharField(verbose_name="Longitude",max_length=50, null=True, blank=True)
-    latitude = models.CharField(verbose_name="Latitude",max_length=50, null=True, blank=True)
 
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(auto_now_add=True)
