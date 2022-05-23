f7313fbe79e6021be0eb354d68e9479f4a32562d
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-23 06:17:51 +0200

    Add view for adding event to route map

--- event/urls.py
+++ event/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("add_event", views.add_event, name="add-event")
 ]
--- event/views.py
+++ event/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import RegisterEventPostForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="/members/login")
+def add_event(request):
+    if request.method == "POST":
+        data = request.POST
+        form = RegisterEventPostForm(data, instance=request.user)
+        # Check if form is valid
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Event created successfully!"))
+            redirect("profile", user_id=request.user.id)
+    context = {
+        "event_form": RegisterEventPostForm
+    }
+    return render(request, "event/add_event.html", context)
