7ac6586d0bae8b25763d50c75434213151ce07fc
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-05-23 04:50:05 +0200

    Register eventpost model in admin

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from event.models import EventPost
 # Register your models here.
+
+class EventPostAdmin(admin.ModelAdmin):
+    list_filter = ["author"]
+    list_display = ["author"]
+    search_fields = ["author"]
+
+    class Meta:
+        model = EventPost
+
+admin.site.register(EventPost, EventPostAdmin)
